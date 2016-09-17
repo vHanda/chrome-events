3d0bca84dbdd91dbded1d9195096b87b3de0a1b3
Vishesh Handa <me@vhanda.in>
2016-09-17 13:16:09 +0200

    Add a function to print total time per website
    It's a bit messy, but it's a decent start to see how easy / hard it will
    be to calculate analytics from this huge event stream.

--- src/main.js
+++ src/main.js
@@ -81,6 +81,14 @@ class Storage {
 }
 
 var storage = new Storage();
+storage.getAll(eventObj => {
+	var events = [];
+	// FIXME: use Object.values
+	for (key in eventObj) {
+		events.push(eventObj[key]);
+	}
+	timeSpentPerSite(events);
+});
 
 // Notes:
 // These events are not perfect and we will need additional events
@@ -91,3 +99,106 @@ var storage = new Storage();
 // Since I do need a local tracker for suspend/resume and for x11
 // Perhaps I can just run a local webserver and send all the events there?
 // That can have the append only log - use nedb-logger
+
+//
+// Analytics
+//
+function timeSpentPerSite(events) {
+	// FIXME: Remove "www."
+	var domains = events.filter(e => e.data && e.data.url).map(e => extractDomain(e.data.url));
+	domains = Array.from(new Set(domains));
+
+	domains.forEach(domain => {
+		// Converts each event into a event which says if we should start / stop
+		// counting the time
+		var domainEvents = events.map(e => {
+			var ne = {
+				time: e.time
+			};
+
+			if (e.type == EVENT_TYPE_TAB_ACTIVATED) {
+				var ed = extractDomain(e.data.url);
+				if (ed == domain) {
+					ne.start = true;
+				} else {
+					ne.stop = true;
+				}
+			}
+
+			if (e.type == EVENT_TYPE_WINDOW_FOCUSED) {
+				var tabs = e.data.tabs;
+				tabs.forEach(tab => {
+					if (!tab.active) {
+						return;
+					}
+					var td = extractDomain(tab.url);
+					if (td == domain) {
+						ne.start = true;
+					} {
+						ne.stop = true;
+					}
+				});
+			}
+
+			if (e.type == EVENT_TYPE_WINDOW_FOCUS_LOST) {
+				ne.start = false;
+			}
+
+			if (e.type == EVENT_TYPE_TAB_UPDATED) {
+				var tab = e.data.tab;
+				if (!tab.active) {
+					return;
+				}
+
+				var td = extractDomain(tab.url);
+				if (td == domain) {
+					ne.start = true;
+				} else {
+					ne.stop = true;
+				}
+			}
+
+			// FIXME: Handle idle time!
+
+			if (!ne.start && !ne.stop) {
+				return;
+			}
+			return ne;
+		});
+		domainEvents = domainEvents.filter(e => e);
+
+		var time = 0;
+		var startTime = null;
+		domainEvents.forEach(e => {
+			if (e.start) {
+				if (!startTime)
+					startTime = e.time;
+			} else {
+				if (!startTime) {
+					return;
+				}
+
+				time += (e.time - startTime);
+				startTime = null;
+			}
+		});
+
+		console.log(domain, time / 1000.0);
+	});
+}
+
+function extractDomain(url) {
+    var domain;
+    //find & remove protocol (http, ftp, etc.) and get domain
+    if (url.indexOf("://") > -1) {
+        domain = url.split('/')[2];
+    }
+    else {
+        domain = url.split('/')[0];
+    }
+
+    //find & remove port number
+    domain = domain.split(':')[0];
+
+    return domain;
+}
