9f6b37edad180ef9b602a4c51305cb6ff6837567
Vishesh Handa <me@vhanda.in>
2018-07-27 01:06:31 +0200

    Remove unused typing
    We are now using the indexed db promises library which seems to provide
    its own typing.

--- src/typing/indexeddb.d.ts
+++
@@ -1,7 +0,0 @@
-// This method seesm to be missing in typescript 2.9.2
-// This has been fixed in typescript 3.0
-// https://github.com/Microsoft/TypeScript/issues/24724
-
-interface IDBObjectStore {
-  getAll(): IDBRequest;
-}
